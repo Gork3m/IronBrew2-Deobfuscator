5b0f03a1655984db2d25baeadc943db7dcf89aaf
Görkem / NT <67007893+Gork3m@users.noreply.github.com>
2021-06-03 14:33:52 +0300

    lets goo

---
+++ IronBrew2 Deobfuscator/CLI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using IronBrew2_Deobfuscator;
+
+namespace CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.Log("[*] Starting..", ConsoleColor.Green);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IronBrew2 Deobfuscator/IronBrew2 Deobfuscator/Debug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IronBrew2_Deobfuscator
+{
+    public static class Debug
+    {
+
+        public static void Log(string msg, ConsoleColor color = ConsoleColor.Gray)
+        {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = old;
+
+        }
+
+
+    }
+}
